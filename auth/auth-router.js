994055e8e005f191fd82c3de16822c372079760c
cjogles <jackson.ogles28@gmail.com>
2019-12-20 09:53:59 -0700

    added auth middleware, added signed token functionality for auth

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
+
+function signToken(user) {
+
+  const payload = {
+    username: user.username,
+  };
+
+  const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
+
+  const options = {
+    expiresIn: "1h",
+  };
+
+  return jwt.sign(payload, secret, options); // notice the return
+}
 
 router.post('/register', (req, res) => {
   // implement registration
@@ -25,7 +41,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${ user.username }!` });
+        const token = signToken(user);
+        res.status(200).json({ token, message: `Welcome ${ user.username }!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
